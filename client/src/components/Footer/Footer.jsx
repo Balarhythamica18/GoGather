347e5956736cae4251d027b752e582ef1fef5d56
Balarythmica18 <balarythmica@gmail.com>
2026-02-27 22:08:18 +0530

    organizer dashbaord change

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Footer.css";
 import logo from "../../assets/logo.png";
 import fb from "../../assets/fb.png";
 import twitter from "../../assets/twitter.png";
-import linkedin from "../../assets/linkedin.png"; 
+import linkedin from "../../assets/linkedin.png";
 
 const Footer = () => {
   return (
@@ -15,61 +16,57 @@ const Footer = () => {
             <img src={logo} alt="GoGather Logo" />
           </div>
           <p className="footer-description">
-            GoGather is a modern self-service event ticketing platform
-            that empowers anyone to create, promote, discover and
-            attend events with ease. From concerts and conferences
-            to workshops and local meetups, GoGather makes event
-            experiences simple, seamless, and accessible for everyone.
+            GoGather is a modern event ticketing platform that empowers
+            communities to create, promote, discover and attend
+            extraordinary events with ease.
           </p>
 
           <div className="footer-socials">
-           <img src={fb} alt="Facebook" />
-           <img src={twitter} alt="Twitter" />
-           <img src={linkedin} alt="LinkedIn" />
+            <img src={fb} alt="Facebook" />
+            <img src={twitter} alt="Twitter" />
+            <img src={linkedin} alt="LinkedIn" />
           </div>
         </div>
 
         {/* Links */}
         <div className="footer-links">
-          <h4>Plan Events</h4>
+          <h4>Explore</h4>
           <ul>
-            <li><a href="#">Create and Set Up</a></li>
-            <li><a href="#">Sell Tickets</a></li>
-            <li><a href="#">Online RSVP</a></li>
-            <li><a href="#">Online Events</a></li>
+            <li><Link to="/events">Browse Events</Link></li>
+            <li><Link to="/my-bookings">My Tickets</Link></li>
+            <li><Link to="/favorites">Favorites</Link></li>
+            <li><Link to="/squad-chat">Squad Chat</Link></li>
           </ul>
         </div>
 
         <div className="footer-links">
-          <h4>Go Gather</h4>
+          <h4>GoGather</h4>
           <ul>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Press</a></li>
-            <li><a href="#">Contact Us</a></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/contact">Contact Us</Link></li>
             <li><a href="#">Help Center</a></li>
-            <li><a href="#">How it Works</a></li>
-            <li><a href="#">Privacy</a></li>
-            <li><a href="#">Terms</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Terms of Service</a></li>
           </ul>
         </div>
 
         {/* Newsletter */}
         <div className="footer-newsletter">
-          <h4>Stay In The Loop</h4>
+          <h4>Stay Connected</h4>
           <p>
-            Join our mailing list to stay in the loop with our newest
-            features and event content.
+            Join our mailing list to stay in the loop with the newest
+            events and community features.
           </p>
 
           <div className="newsletter-form">
-            <input type="email" placeholder="Enter your email address" />
+            <input type="email" placeholder="Enter your email" />
             <button>Subscribe Now</button>
           </div>
         </div>
       </div>
 
       <div className="footer-bottom">
-        Copyright © 2025 Balarhythamica
+        Copyright © {new Date().getFullYear()} Balarhythamica · GoGather
       </div>
     </footer>
   );
