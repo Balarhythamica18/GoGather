4e420fea5d59309fb0b25ea3b9fd1286ce4b9017
Balarhythamica18 <165568551+Balarhythamica18@users.noreply.github.com>
2026-02-27 22:39:37 +0530

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
 // Centralized socket connection
-const socket = io("http://localhost:5000");
+const socket = io("https://gogather-server.onrender.com");
 
 export default socket;
